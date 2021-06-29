3908c2905823c292ea106b1745d2e392a520e4c6
Alexsander Genuino <genuinoalexsander@gmail.com>
2021-06-28 23:57:28 -0300

    footer atualização

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,86 @@
 import React from 'react';
-import { Container, TextFooter } from './styles';
+import { Container, 
+  TextFooter, 
+  RodapeStyle, 
+  StyleFooter, 
+  ContentHistory, 
+  TextHistory, 
+  StyleSVG, 
+  Contato, 
+  Services,  
+  StyleContact,
+  Titulo,
+  Item,
+  ItemService,
+  ServiceText} from './styles';
+import LogoPNG from '../../assets/CDGLOGO.png';
+import { AiOutlineWhatsApp, AiFillInstagram, AiOutlineFacebook} from "react-icons/ai"
+
+    const handlerlinkclick = (Link) =>{
+      window.open(Link, '_blank')
+    }
+
+    const LinkWhats = "http://wa.me/5519993176589"
+    const LinkFace = "https://www.facebook.com/cdgcontabildadeeassessoria"
+    const LinkInsta = "https://www.instagram.com/cdgcontabilidadeassessoria/"
 
 const Footer = () => {
   return (
   
     <Container>
-        <TextFooter>2021 - Todos os direitos reservados</TextFooter>
+        <StyleFooter>
+          <ContentHistory>
+              <img src={LogoPNG} style={{
+                width: 120,
+                height: 50,}}>
+              </img>
+          <StyleSVG>
+            <AiOutlineWhatsApp onClick = { () => handlerlinkclick(LinkWhats)}/>
+            <AiFillInstagram onClick = { () => handlerlinkclick(LinkInsta)}/>
+            <AiOutlineFacebook onClick = { () => handlerlinkclick(LinkFace)}/>
+          </StyleSVG>
+          <div style={{width:'50%'}}>
+            <TextHistory>Buscamos sempre a melhoria contínua dos nossos serviços oferecendo uma gestão contábil, fiscal e trabalhista de excelência, e, além de consultoria nessas áreas, também buscamos prestar serviços de qualidade através de planejamento tributário.</TextHistory>
+          </div>
+          </ContentHistory>
+          <StyleContact>
+            <Contato>
+                <Titulo>Contato</Titulo>
+                <Item>Rua Pedro Galdino Moreira, 142 | Jardim Paulista | Monte Mor | SP</Item>
+                <Item>(19) 3879-5995</Item>
+                <Item>(19) 99317-6589</Item>
+                <Item>contato@cdgcontabilidade.com.br</Item>
+            </Contato>
+            <Services>
+                <ItemService>
+                  <Titulo>Serviços</Titulo>
+                  <ServiceText>Contabilidade</ServiceText>
+                  <ServiceText>Fiscal e Tributario</ServiceText>
+                  <ServiceText>Administração de Pessoal</ServiceText>
+                  <ServiceText>Consultoria</ServiceText>
+                </ItemService>
+                <ItemService>
+                  <Titulo>Navegação</Titulo>
+                  <ServiceText>Home</ServiceText>
+                  <ServiceText>Nossas Soluções</ServiceText>
+                  <ServiceText>Quem Somos</ServiceText>
+                  <ServiceText>Contato</ServiceText>
+                </ItemService>
+                <ItemService>
+                  <Titulo>Especilidades</Titulo>
+                  <ServiceText>Prestadores de Serviços</ServiceText>
+                  <ServiceText>Comércio</ServiceText>
+                  <ServiceText>Indústia</ServiceText>
+                  <ServiceText>Construção Civil</ServiceText>
+                </ItemService>
+            </Services>
+          </StyleContact>
+        </StyleFooter>
+        <RodapeStyle>
+          <TextFooter>Contabilidade em Monte Mor - SP</TextFooter>
+          <TextFooter>2021 - Todos os direitos reservados</TextFooter>
+          <TextFooter>Desenvolvido por</TextFooter>
+        </RodapeStyle>
     </Container>
   
   );
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,20 +2,153 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 width: 100%;
-height: 8vh;
+height: 60vh;
 background: #F9F3F3;
 margin-top:3%;
+`;
+
+export const ContentHistory = styled.div`
+width: 30%;
+height: 90%;
+display: flex;
+align-items: center;
+flex-direction: column;
+justify-content: space-around;
+border-right: 2px solid;
+border-right-color: #BABABA;
+
+`;
+
+export const StyleSVG = styled.div`
+width: 30%;
+display: flex;
+align-items: center;
+justify-content: space-around;
+
+
+svg{
+    width: 1.5rem;
+    height: 1.5rem;
+    color: #470606;
+    cursor: pointer;
+    :hover{
+        color: #BABABA;
+    }
+}
+
+`;
+
+export const StyleContact = styled.div`
+
+width: 90%;
+height: 90%;
+display: flex;
+align-items: center;
+flex-direction: column;
+justify-content: center;
+
+`;
+
+export const Contato = styled.div`
+width: 95%;
+height: 50%;
+display:flex;
+flex-direction: column;
+align-items: center;
+
+`;
+
+export const Services = styled.div`
+width: 95%;
+height: 50%;
+display: flex;
+flex-direction: row;
+`;
+
+export const ItemService = styled.div`
+height: 100%;
+width: 30%;
+display: flex;
+flex-direction: column;
+align-items: center;
+
+`;
+
+export const RodapeStyle = styled.div`
+width: 100%;
+height: 10%;
 display:flex;
 align-items:center;
+background:#E9DFDF;
+`;
+
+export const StyleFooter = styled.div`
+width: 100%;
+height: 90%;
+display:flex;
+align-items:center;
+background:#F9F3F3;
+
 `;
 
 export const TextFooter = styled.h1`
 font-family: 'M PLUS 1p';
 font-style: normal;
-font-weight: 500;
-font-size: 14px;
+font-weight: 400;
+font-size: 0.8rem;
 line-height: 21px;
 color: #000000;
 margin-left:15%;
 
 `;
+
+export const TextHistory = styled.h1`
+font-family: 'M PLUS 1p';
+font-style: initial;
+font-weight: 300;
+font-size: 0.8rem;
+line-height: 1.5rem;
+color: #000000;
+text-align: center;
+
+`;
+
+export const Titulo = styled.h1`
+font-family: 'M PLUS 1p';
+font-style: Bold;
+font-weight: 800;
+font-size: 1.0rem;
+line-height: 1.5rem;
+color: #474747;
+text-align: center;
+align-self: flex-start;
+padding: 1%;
+
+`;
+
+export const Item = styled.a`
+font-family: 'M PLUS 1p';
+font-style: initial;
+font-weight: 300;
+font-size: 0.8rem;
+line-height: 1.5rem;
+color: #000000;
+text-align: center;
+align-self: flex-start;
+`;
+
+export const ServiceText = styled.a`
+font-family: 'M PLUS 1p';
+font-style: initial;
+font-weight: 300;
+font-size: 0.8rem;
+line-height: 1.5rem;
+color: #000000;
+text-align: center;
+align-self: flex-start;
+:hover{
+    color:#E9DFDF;
+    cursor: pointer;
+}
+`;
+
